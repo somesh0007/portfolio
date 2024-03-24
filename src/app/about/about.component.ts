ad81d9c2e05c452f50f9d9eeab60eee593f68663
Boston Luzxor Limo <someshgusain.0007@gmail.com>
2024-03-24 04:24:33 -0400

    changes in about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,7 +9,8 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
   title: string = "Hello, my name is Somesh ";
-  paragraph: string = `Skilled Angular Developer with 2+ years of hands-on experience, adept at creating scalable web applications. Proficient in Angular, JavaScript, HTML, CSS, with expertise in developing single-page applications (SPAs) using Angular.`;
+  paragraph: string = `Skilled Angular Developer with 2+ years of hands-on experience, 
+  adept at creating scalable web applications. Proficient in Angular, JavaScript, HTML, CSS with expertise in developing single-page applications (SPAs) using Angular.`;
 
   ngOnInit() {}
 
